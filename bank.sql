b2e4cec97a07492ce2a3a235e233cdf148d81784
keerti123235 <76983881+keerti123235@users.noreply.github.com>
2021-09-18 17:08:21 +0530

    Add files via upload

---
+++ bank.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 18, 2021 at 04:55 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `balance` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `Name`, `email`, `balance`) VALUES
+(1, 'Keerti', 'keerti@gmail.com', 160500),
+(2, 'Raj', 'Raj@gmail.com', 20000),
+(3, 'Jay', 'Jay@gmail.com', 63500),
+(4, 'Shreya','shreya@gmail.com',919000),
+(5, 'Praveen','praveen@gmail.com'89000),
+(6, 'Sanjay','sanjay@gmail.com',79000),
+(7,'Divya','Divya@gmail.com',87600),
+(8, 'Janvi', 'Janvi@gmail.com', 15000),
+(9,'Sanjana','Sanjana@gmail.com',986700),
+(10,'Amrutha','Amrutha@gmail.com',670000)
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `his_id` int(11) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `reciever` varchar(255) NOT NULL,
+  `amount` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`his_id`, `sender`, `reciever`, `amount`) VALUES
+(8, 'Keerti', 'Amrutha', 600);
+(9,'Jay','Keerti',500);
+(10,'Shreya','Sanjay',1000);
+(11,'Divya','Amrutha',250);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`his_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `his_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
